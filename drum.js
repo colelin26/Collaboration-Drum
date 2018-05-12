40029280792263231937ea76f96b45f53e1d59f4
Cole <d43lin@edu.uwaterloo.ca>
2018-05-12 02:08:11 -0400

    enables multiple keys controlling the same sound

--- drum.js
+++ drum.js
@@ -2,28 +2,45 @@ window.addEventListener('keydown', playSound)
 window.addEventListener('click', changeButton)
 
 var change = false; // indicates if we need to change the key of the buttom
-var keyNeedChange;//
+var id;
 
 function playSound(e) {
   if (change === false) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    const audio = document.querySelectorAll(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelectorAll(`.key[data-key="${e.keyCode}"]`);
       if(!audio) return;//
-      audio.currentTime=0;//
-      audio.play();
-      key.classList.add('playing');
+      audio.forEach(function (each) {
+        each.currentTime=0;//
+        each.play();
+      });
+      key.forEach(function (each){
+        each.classList.add('playing');
+      });
     } else {
       console.log(String.fromCharCode(e.keyCode));
-      console.log(keyNeedChange);
-      keyNeedChange.innerHTML=`<kbd>${String.fromCharCode(e.keyCode)}</kbd>`;
+      console.log(id);
       change = false;
+      let btn = document.querySelector(`#${id}`);
+      btn.innerHTML=`<kbd>${String.fromCharCode(e.keyCode)}</kbd>`;
+
+      let dataKey = btn.parentElement.attributes[0].value;
+      console.log(dataKey);
+
+      const audio = document.querySelector(`audio[data-key="${dataKey}"]`);
+      const key = document.querySelector(`.key[data-key="${dataKey}"]`);
+      audio.setAttribute("data-key", e.keyCode);
+      key.setAttribute("data-key", e.keyCode);
     }
-}
+  }
+
 
 function changeButton(btn) {
   change = true;
-  keyNeedChange = btn;
-  btn.innerHTML q="<kbd>?</kbd>";
+  console.log(btn);
+  var backup = id;
+  id = btn.id;
+  if (id === undefined) id = backup;
+  btn.innerHTML="?";
 }
 
 // ${String.fromCharCode(e.keyCode)}
