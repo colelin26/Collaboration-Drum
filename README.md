69ef44bdf86da840751c505b3e540cdcbd468a43
Cole <d43lin@edu.uwaterloo.ca>
2018-05-24 01:41:32 -0400

    added keystroke-recorder

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ A real-time online drum kit powered by Express.js and Socket.io.
 
 ## Install
 
-1. Run ```node app.js``` in your shell
+
+1.  ```node app.js``` in your shell
 2. Visit http://localhost:5000 in your browser.
 
 ## Instruction
 
 1. Press the keys on the keyboards to play the drum!
 2. Click the buttoms to edit the keys.
-
--- app.js
+++ app.js
@@ -4,11 +4,39 @@ var http = require('http');
 var path = require('path');
 var socketIO = require('socket.io');
 var app = express();
+// mongodb
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/collaboration-drum");
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 var server = http.Server(app);
 var io = socketIO(server);
 var currentlyOnline = 0;
 app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
+
+// Schema
+var nameSchema = new mongoose.Schema({
+  firstName: String,
+  lastNameName: String
+});
+
+var User = mongoose.model("User", nameSchema);
+
+// Saving data to database
+app.post("/addname", (req, res) => {
+  var myData = new User(req.body);
+  myData.save()
+    .then(item => {
+      res.send("item saved to database");
+    })
+    .catch(err => {
+      res.status(400).send("unable to save to database");
+    });
+});
 
 // Routing
 app.get('/', function(req, res) {
